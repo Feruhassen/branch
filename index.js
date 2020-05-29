c86cf542c206101ff68c6635d3e79fdf1bbc4259
mulu Hassen <emanbedru1@outlook.com>
2020-05-29 15:25:54 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//document.getElementByid("header");
+//console.log(document.getElementById("header"));
+//document.QuarySelector(".heading");
+//console.log(document.querySelector(".head"));
+//document.getElementsByClassName("list");
+//console.log(document.getElementsByClassName("list"));
+//document.querySelectorAll("li")
+console.log(document.querySelectorAll("li"));
